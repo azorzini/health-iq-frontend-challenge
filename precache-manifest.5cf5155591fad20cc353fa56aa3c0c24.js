2e259eb4daf2bbae2b0943135c13ef49d05a3e36
Alvaro Zorzini <azorzini@avioconsultig.com>
2020-07-13 23:48:03 -0300

    Updates

---
+++ precache-manifest.5cf5155591fad20cc353fa56aa3c0c24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac13510bb2eadf720e9f9feb5e8256f",
+    "url": "/health-iq-frontend-challenge/index.html"
+  },
+  {
+    "revision": "8897ea4fa68c4feb9451",
+    "url": "/health-iq-frontend-challenge/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8897ea4fa68c4feb9451",
+    "url": "/health-iq-frontend-challenge/static/js/2.212d65e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/health-iq-frontend-challenge/static/js/2.212d65e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2779f87bcc12bcbe53c6",
+    "url": "/health-iq-frontend-challenge/static/js/main.a4fecedf.chunk.js"
+  },
+  {
+    "revision": "0d531ca9761885256cb8",
+    "url": "/health-iq-frontend-challenge/static/js/runtime-main.03857a58.js"
+  }
+]);
