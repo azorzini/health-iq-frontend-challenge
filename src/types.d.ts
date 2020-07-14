7870db3cfa5b1949fde4a5d8986f9e2ff0048373
Alvaro Zorzini <azorzini@avioconsultig.com>
2020-07-13 23:32:47 -0300

    chore: add types

---
+++ src/types.d.ts
@@ -0,0 +1,43 @@
+export interface PostsWithUser {
+  title: string;
+  body: string;
+  username: string;
+  city: string;
+}
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
